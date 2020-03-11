c42f11404aa34f879c624f818fa00af5e88cea2d
Kexin <kexinhao01111993@gmail.com>
2020-03-11 01:02:31 +0100

    release day

---
+++ mainpage/hyperlinks/slideshow/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $("#showVids").click(function(){
+    location.href = "../vid/index.html";
+  });
+  $("#showInfo").click(function(){
+    location.href = "../info/index.html";
+  });
+  $("#showPics").click(function(){
+    location.href = "../slideshow/index.html";
+  });
+  $("#backHome").click(function(){
+    location.href = "../../../index.html";
+  });
+});
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
