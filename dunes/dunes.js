5cd14a0b6577b06580228ec41d0cef29b0663fae
Kexin <kexinhao01111993@gmail.com>
2020-04-20 21:56:19 +0200

    dunes

---
+++ dunes/dunes.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.content>img').click(function(){
+    location.href = "video.html";
+  });
+  $("#episode1").click(function(){
+    location.href = "../tunnel/video.html";
+  });
+  $("#episode2").click(function(){
+    location.href = "video.html";
+  });
+});
---
+++ dunes/photo.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#showVids").click(function(){
+    location.href = "video.html#vid";
+  });
+  $("#showInfo").click(function(){
+    location.href = "video.html#info";
+  });
+  $("#showPics").click(function(){
+    location.href = "photo.html";
+  });
+  $("#backHome").click(function(){
+    location.href = "tunnel.html";
+  });
+});
