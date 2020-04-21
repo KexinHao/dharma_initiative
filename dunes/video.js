48cf6c412d1e0da5df40e93f0098c54e406ec6d7
Kexin <kexinhao01111993@gmail.com>
2020-04-21 13:48:46 +0200

    fin

--- dunes/video.js
+++ dunes/video.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+  $("#showvideo").click(function(){
+    location.href = "video.html";
+  });
+  $("#showinfo").click(function(){
+    location.href = "info.html";
+  });
+  $("#showpics").click(function(){
+    location.href = "photo.html";
+  });
+  $("#backhome").click(function(){
+    location.href = "dunes.html";
+  });
+});
