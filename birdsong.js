4584ca3a28bd6fcaae15aaf0d1935dc9ad3abe08
Kexin <kexinhao01111993@gmail.com>
2020-06-05 14:41:36 +0200

    birdsong update

---
+++ birdsong.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#backhome").click(function(){
+    location.href = "birdsong.html";
+  });
+  $("#instagram").click(function(){
+    location.href = "https://www.instagram.com/dharma_initiatives/";
+  });
+  $("#soundcloud").click(function(){
+    location.href = "https://soundcloud.com/dharmainitiatives";
+  });
+  $("#episode1").click(function(){
+    location.href = "../tunnel/tunnel.html";
+  });
+  $("#episode2").click(function(){
+    location.href = "../dunes/dunes.html";
+  });
+  $("#episode3").click(function(){
+    location.href = "birdsong.html";
+  });
+});
